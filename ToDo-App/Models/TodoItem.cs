a30b8628d4ece5b6536a3cdf1c498ffecbdedaeb
bodorkost <tibor.bodorkos@jaystack.com>
2019-02-11 15:17:39 +0100

    Create TodoItem model

---
+++ ToDo-App/Models/TodoItem.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ToDo_App.Models
+{
+    public enum Category
+    {
+        BUG,
+        TASK,
+        EPIC
+    }
+
+    public class TodoItem
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Priority { get; set; }
+        public string Responsible { get; set; }
+        public DateTime Deadline { get; set; }
+        public int Status { get; set; }
+        public Category Category { get; set; }
+        public Guid PareintId { get; set; }
+    }
+}
