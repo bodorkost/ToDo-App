1ac14cb7a8d07c2f7008e0c127eb88c3fc50a29a
bodorkost <tibor.bodorkos@jaystack.com>
2019-02-11 14:50:08 +0100

    Inits project

---
+++ ToDo-App/Controllers/TodosController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ToDo_App.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TodosController : ControllerBase
+    {
+        // GET api/todos
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+    }
+}
