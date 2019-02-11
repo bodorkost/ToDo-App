0f259128ceca0f9d594e842977785a2e7f5a99f8
bodorkost <tibor.bodorkos@jaystack.com>
2019-02-11 15:27:49 +0100

    Create mock list of items

--- ToDo-App/Controllers/TodosController.cs
+++ ToDo-App/Controllers/TodosController.cs
@@ -1,8 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using ToDo_App.Models;
 
 namespace ToDo_App.Controllers
 {
@@ -10,6 +9,51 @@ namespace ToDo_App.Controllers
     [ApiController]
     public class TodosController : ControllerBase
     {
+        public static IList<TodoItem> _items { get; set; }
+
+        public TodosController()
+        {
+            _items = new List<TodoItem>
+            {
+                new TodoItem()
+                {
+                    Id = Guid.NewGuid(),
+                    Name = "First",
+                    Description = "This is the first todo in my list.",
+                    Priority = 1,
+                    Responsible = "?",
+                    Deadline = new DateTime(2020, 01, 01),
+                    Status = 0,
+                    Category = Category.BUG,
+                    ParentId = null
+                },
+                new TodoItem()
+                {
+                    Id = Guid.NewGuid(),
+                    Name = "Second",
+                    Description = "This is the second todo in my list.",
+                    Priority = 2,
+                    Responsible = "?",
+                    Deadline = new DateTime(2019, 05, 16),
+                    Status = 0,
+                    Category = Category.EPIC,
+                    ParentId = null
+                },
+                new TodoItem()
+                {
+                    Id = Guid.NewGuid(),
+                    Name = "Third",
+                    Description = "This is the third todo in my list.",
+                    Priority = 3,
+                    Responsible = "?",
+                    Deadline = new DateTime(2019, 12, 12),
+                    Status = 0,
+                    Category = Category.TASK,
+                    ParentId = null
+                }
+            };
+        }
+
         // GET api/status
         [HttpGet]
         public IActionResult Status()
