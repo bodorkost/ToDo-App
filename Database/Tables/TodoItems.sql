f68800e28b85d8d16b8a931e7cec333b86c8aa57
bodorkost <tibor.bodorkos@jaystack.com>
2019-02-13 11:45:40 +0100

    Create database and table from SQL project

---
+++ Database/Tables/TodoItems.sql
@@ -0,0 +1,17 @@
+﻿CREATE TABLE [dbo].[TodoItems]
+(
+	[Id] UNIQUEIDENTIFIER NOT NULL PRIMARY KEY, 
+    [Name] NVARCHAR(50) NOT NULL, 
+    [Description] NVARCHAR(500) NULL, 
+    [Priority] INT NOT NULL, 
+    [Responsible] NVARCHAR(50) NULL, 
+    [Deadline] DATETIME NULL, 
+    [Status] INT NULL, 
+    [Category] INT NULL, 
+    [ParentId] UNIQUEIDENTIFIER NULL, 
+    [Created] DATETIME NOT NULL, 
+    [Creator] NVARCHAR(50) NOT NULL, 
+    [Modified] DATETIME NOT NULL, 
+    [Modifier] NVARCHAR(50) NOT NULL, 
+    [IsDeleted] BIT NOT NULL DEFAULT 0
+)
