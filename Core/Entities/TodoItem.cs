33d26b57969305577149b4c39244a9af7adf0155
bodorkost <tibor.bodorkos@jaystack.com>
2019-02-13 11:30:01 +0100

    Refact restrictions to model annotation

--- Core/Entities/TodoItem.cs
+++ Core/Entities/TodoItem.cs
@@ -1,23 +1,53 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 using Core.Types;
 
 namespace Core.Models
 {
     public class TodoItem
     {
+        [Required]
         public Guid Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
         public string Name { get; set; }
+
+        [StringLength(500)]
         public string Description { get; set; }
+
+        [Required]
         public Priority Priority { get; set; }
+
+        [StringLength(50)]
         public string Responsible { get; set; }
+
+        [DataType(DataType.DateTime)]
         public DateTime Deadline { get; set; }
+
         public int Status { get; set; }
+
         public Category Category { get; set; }
+
         public Guid? ParentId { get; set; }
+
+        [Required]
+        [DataType(DataType.DateTime)]
         public DateTime Created { get; set; }
+
+        [Required]
+        [StringLength(50)]
         public string Creator { get; set; }
+
+        [Required]
+        [DataType(DataType.DateTime)]
         public DateTime Modified { get; set; }
+
+        [Required]
+        [StringLength(50)]
         public string Modifier { get; set; }
+
+        [Required]
         public bool IsDeleted { get; set; }
     }
 }
--- Infrastructure/Data/TodoContext.cs
+++ Infrastructure/Data/TodoContext.cs
@@ -1,6 +1,5 @@
 ﻿using Core.Models;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace Infrastructure.Data
 {
@@ -11,48 +10,5 @@ namespace Infrastructure.Data
         { }
 
         public DbSet<TodoItem> TodoItems { get; set; }
-
-        protected override void OnModelCreating(ModelBuilder builder)
-        {
-            builder.Entity<TodoItem>(ConfigureTodoItem);
-        }
-
-        private void ConfigureTodoItem(EntityTypeBuilder<TodoItem> builder)
-        {
-            builder
-                .HasKey(t => t.Id)
-                .HasName("PrimaryKey_TodoItemId");
-
-            builder.Property(t => t.Name)
-                .HasMaxLength(50)
-                .IsRequired();
-
-            builder.Property(t => t.Description)
-                .HasMaxLength(500);
-
-            builder.Property(t => t.Priority)
-                .IsRequired();
-
-            builder.Property(t => t.Responsible)
-                .HasMaxLength(50);
-
-            builder.Property(t => t.Created)
-                .IsRequired();
-
-            builder.Property(t => t.Creator)
-                .HasMaxLength(50)
-                .IsRequired();
-
-            builder.Property(t => t.Modified)
-                .IsRequired();
-
-            builder.Property(t => t.Modifier)
-                .HasMaxLength(50)
-                .IsRequired();
-
-            builder.Property(t => t.IsDeleted)
-                .HasDefaultValue(false)
-                .IsRequired();
-        }
     }
 }
