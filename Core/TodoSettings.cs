9788f98d6913eb15cbc1e40f795d1ecfd2c7be55
bodorkost <tibor.bodorkos@jaystack.com>
2019-02-14 14:24:59 +0100

    Create TodoSettings config

---
+++ Core/TodoSettings.cs
@@ -0,0 +1,7 @@
+﻿namespace Core
+{
+    public class TodoSettings
+    {
+        public int RecentHours { get; set; }
+    }
+}
--- ToDo-App/Startup.cs
+++ ToDo-App/Startup.cs
@@ -1,19 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Infrastructure.Interfaces;
+﻿using Infrastructure.Interfaces;
 using Infrastructure.Data;
 using Infrastructure.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
+using Core;
 
 namespace ToDo_App
 {
@@ -32,6 +26,9 @@ namespace ToDo_App
             services.AddDbContext<TodoContext>(options =>
                 options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"))
             );
+
+            services.AddOptions();
+            services.Configure<TodoSettings>(Configuration);
 
             services.AddHealthChecks();
             services.AddCors();
