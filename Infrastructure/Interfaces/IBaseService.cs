2c48c565b235b05ace634237c3ad6b1348223f97
bodorkost <tibor.bodorkos@jaystack.com>
2019-02-15 16:47:55 +0100

    Create BaseService with interface

---
+++ Infrastructure/Interfaces/IBaseService.cs
@@ -0,0 +1,19 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+
+namespace Infrastructure.Interfaces
+{
+    public interface IBaseService<T> where T : BaseEntity
+    {
+        T Create(T item);
+
+        T GetById(Guid id);
+
+        IEnumerable<T> GetAll();
+
+        T Edit(Guid id, T item);
+
+        T Delete(Guid id);
+    }
+}
---
+++ Infrastructure/Services/BaseService.cs
@@ -0,0 +1,64 @@
+﻿using Core.Entities;
+using Infrastructure.Data;
+using Infrastructure.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Infrastructure.Services
+{
+    public abstract class BaseService<T> : IBaseService<T> where T : BaseEntity
+    {
+        protected readonly TodoContext _dbContext;
+
+        public BaseService(TodoContext context)
+        {
+            _dbContext = context;
+        }
+
+        public T Create(T item)
+        {
+            item.Id = Guid.NewGuid();
+            item.Created = DateTime.Now;
+            //TODO entity.CreatedById 
+            item.Modified = DateTime.Now;
+            //TODO entity.ModifiedById 
+
+            _dbContext.Set<T>().Add(item);
+            _dbContext.SaveChanges();
+
+            return item;
+        }
+
+        public T Delete(Guid id)
+        {
+            var item = GetById(id);
+            if (item == null)
+            {
+                return null;
+            }
+
+            item.IsDeleted = true;
+            item.Deleted = DateTime.Now;
+            //TODO item.DeletedById
+
+            _dbContext.Entry(item).State = EntityState.Modified;
+            _dbContext.SaveChanges();
+
+            return item;
+        }
+
+        public abstract T Edit(Guid id, T item);
+
+        public IEnumerable<T> GetAll()
+        {
+            return _dbContext.Set<T>().AsEnumerable();
+        }
+
+        public T GetById(Guid id)
+        {
+            return _dbContext.Set<T>().Find(id);
+        }
+    }
+}
